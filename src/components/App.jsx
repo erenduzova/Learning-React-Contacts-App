e66d14c37f3f74cca1417e4bdfe9bd4687894fff
erenduzova <90982361+erenduzova@users.noreply.github.com>
2022-06-06 20:44:58 +0300

    Card component created Extract the contact card as a reusable Card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 
+import Card from "./Card.jsx";
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        mail="b@beyonce.com"
+      />
     </div>
   );
 }
