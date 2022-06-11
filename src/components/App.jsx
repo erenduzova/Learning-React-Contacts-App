016af4997174031591e86410c0bac8ceb3d4d826
erenduzova <90982361+erenduzova@users.noreply.github.com>
2022-06-12 01:25:44 +0300

    map function and id added For better code map function added to create Cards and id added to every contact for key prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,25 @@ import Card from "./Card.jsx";
 import contacts from "../contacts.js";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
-  let cards = [];
-  contacts.forEach((contact) => {
-    cards.push(
-      <Card
-        name={contact.name}
-        imgURL={contact.imgURL}
-        phone={contact.phone}
-        email={contact.email}
-      />
-    );
-  });
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQHgUV-N80Vpmw/profile-displayphoto-shrink_200_200/0/1581514808020?e=1659571200&v=beta&t=qK2ySDu-ZS6M36AjyDM0v-w813XezERVrzMJYrygnAs" />
-      {cards}
+      {contacts.map(createCard)}
     </div>
   );
 }
