39f26210c13b567668fa3db5dc04292f49093f0b
erenduzova <90982361+erenduzova@users.noreply.github.com>
2022-06-15 00:03:44 +0300

    Simplified App.jsx App.jsx component simplified with arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,21 @@ import Card from "./Card.jsx";
 import contacts from "../contacts.js";
 import Avatar from "./Avatar";
 
-function createCard(contact) {
-  return (
-    <Card
-      key={contact.id}
-      id={contact.id}
-      name={contact.name}
-      imgURL={contact.imgURL}
-      phone={contact.phone}
-      email={contact.email}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQHgUV-N80Vpmw/profile-displayphoto-shrink_200_200/0/1581514808020?e=1659571200&v=beta&t=qK2ySDu-ZS6M36AjyDM0v-w813XezERVrzMJYrygnAs" />
-      {contacts.map(createCard)}
+      {contacts.map((contact) => (
+        <Card
+          key={contact.id}
+          id={contact.id}
+          name={contact.name}
+          imgURL={contact.imgURL}
+          phone={contact.phone}
+          email={contact.email}
+        />
+      ))}
     </div>
   );
 }
